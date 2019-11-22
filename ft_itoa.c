030f2650d57960be7459e46fb64f4d8bc983c69b
Sophie CARBONI <scarboni@e1r9p5.42.fr>
2019-11-22 15:45:41 +0100

    save before log out

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,57 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 10:38:18 by scarboni          #+#    #+#             */
-/*   Updated: 2019/11/22 10:02:40 by scarboni         ###   ########.fr       */
+/*   Updated: 2019/11/22 15:45:19 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static size_t	strlen_from_int(int value)
+{
+	size_t result;
+
+	result = 1;
+	if(value < 0)
+		result++;
+	else if(value == 0)
+		return 2;
+	while (value != 0)
+	{
+		value = value / 10;
+		result++;
+	}
+	return (result);
+}
+
 
 char	*ft_itoa(int n)
 {
+	char *str;
+	int i;
+	int pow;
 	(void)n;
-	return NULL;
+	size_t len;
+
+	i = 0;
+	len = strlen_from_int(n);
+	printf("ITOA [%d][%d]\n", n, strlen_from_int(n));
+	str = (char*)malloc(sizeof(char) * len);
+	if(!str)
+		return NULL;
+	if(n < 0)
+	{
+		str[i] = '-';
+		i++;
+	}
+	while(i < len)
+	{
+		str[len - po] = '-';
+		
+	}
+
+	str[i] = '\0';
+	printf("RESULT [%s]\n", str);
+	return str;
 }
