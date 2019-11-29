3677970116bab3b7b46b1ae3e66488a73171d389
Sophie CARBONI <scarboni@e1r12p3.42.fr>
2019-11-29 14:29:27 +0100

    protecting lstiter

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 10:38:18 by scarboni          #+#    #+#             */
-/*   Updated: 2019/11/25 15:50:15 by scarboni         ###   ########.fr       */
+/*   Updated: 2019/11/29 14:28:59 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (!lst)
+	if (!lst || !f)
 		return ;
 	ft_lstiter(lst->next, f);
 	(*f)(lst->content);
