2380c0789d3a1fe3ef9a5d1ff33016f19d550521
Sophie CARBONI <scarboni@e1r13p4.42.fr>
2020-01-07 17:13:46 +0100

    stdio get out

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,11 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 10:38:18 by scarboni          #+#    #+#             */
-/*   Updated: 2020/01/07 14:50:13 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/01/07 17:07:59 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static void	init(size_t *j, int *start_pattern, size_t *i)
 {
