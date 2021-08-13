54e6e3d367985f4647d5068af8d753925285ea16
scarboni <socarboni@gmail.com>
2021-08-13 09:58:03 +0200

    fixing bad function name

--- ft_lstiter_fun_first.c
+++ ft_lstiter_fun_first.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 10:38:18 by scarboni          #+#    #+#             */
-/*   Updated: 2021/08/13 01:10:15 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/08/13 09:46:33 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	ft_lstiter_fun_first(t_list *lst, void (*f)(void *))
 	if (!lst || !f)
 		return ;
 	(*f)(lst->content);
-	ft_lstiter(lst->next, f);
+	ft_lstiter_fun_first(lst->next, f);
 }
