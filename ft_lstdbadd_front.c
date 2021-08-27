bdbf7c7f541b65009a8f75e0a5a8e432f4b17bbd
scarboni <socarboni@gmail.com>
2021-08-27 15:51:13 +0200

    fixing db list

--- ft_lstdbadd_front.c
+++ ft_lstdbadd_front.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 10:38:18 by scarboni          #+#    #+#             */
-/*   Updated: 2021/08/27 11:31:57 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/08/27 15:22:01 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_lstdbadd_front(t_list_double **alst, t_list_double *new)
 {
 	if (!new || !alst)
 		return ;
+	if (*alst)
+		(*alst)->prev = new;
 	new->next = *alst;
 	*alst = new;
 }
